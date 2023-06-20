d60c711232c2e5a2b72123cb4e60ac7dd9b1c19a
Dimitris Milios <dimitris.milios96@gmail.com>
2023-06-20 13:39:47 +0300

    feat: add links component

---
+++ src/lib/commands/Links.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  const links = [
+    {
+      name: "GitHub",
+      url: "https://github.com/DimMilios",
+      urlText: "github/DimMilios",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://linkedin.com/in/dimitris-milios",
+      urlText: "linkedin/dimitris-milios",
+    },
+  ];
+</script>
+
+<p>You can find me at:</p>
+
+<div class="socials">
+  {#each links as { name, url, urlText }, i}
+    <span class="text-primary">{i + 1}. {name}</span>
+    <span class="link">
+      <span>- </span>
+      <a class="link-secondary" href={url} target="_blank">{urlText}</a></span
+    >
+  {/each}
+</div>
+
+<style>
+  p {
+    margin: 0.2rem 0 0.7rem 0;
+  }
+
+  .socials {
+    display: grid;
+    grid-template-columns: 15ch 1fr;
+  }
+
+  .link {
+    margin-bottom: 0.4rem;
+  }
+</style>
--- src/lib/components/Output.svelte
+++ src/lib/components/Output.svelte
@@ -4,6 +4,8 @@
   import Welcome from "../commands/Welcome.svelte";
   import type { CommandKey } from "../types";
   import About from "../commands/About.svelte";
+  import Links from "../commands/Links.svelte";
+  import Education from "../commands/Education.svelte";
 
   type Option = {
     command: CommandKey;
@@ -16,6 +18,8 @@
     { command: "help", component: Help },
     { command: "welcome", component: Welcome },
     { command: "about", component: About },
+    { command: "links", component: Links },
+    { command: "education", component: Education },
     { command: "", component: null },
   ];
 
