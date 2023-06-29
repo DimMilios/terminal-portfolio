e34a536519e1bd9cc1651ca928542e0ddd5148ae
DimMilios <elles1992@yahoo.gr>
2023-06-29 16:22:52 +0300

    feat: group skills by category

--- src/lib/commands/Skills.svelte
+++ src/lib/commands/Skills.svelte
@@ -1,54 +1,39 @@
 <script lang="ts">
+  import SkillsList from '../components/SkillsList.svelte';
+
   const skills = [
-    { skill: 'angular', icon: 'angular.svg' },
-    { skill: 'css', icon: 'css3.svg' },
-    { skill: 'docker', icon: 'docker.svg' },
-    { skill: 'express', icon: 'express.svg' },
-    { skill: 'git', icon: 'git.svg' },
-    { skill: 'github', icon: 'github.svg' },
-    { skill: 'go', icon: 'go.svg' },
-    { skill: 'html', icon: 'html5.svg' },
-    { skill: 'java', icon: 'java.svg' },
-    { skill: 'javascript', icon: 'javascript.svg' },
-    { skill: 'linux', icon: 'linux.svg' },
-    { skill: 'mysql', icon: 'mysql.svg' },
-    { skill: 'nodejs', icon: 'nodejs.svg' },
-    { skill: 'postgresql', icon: 'postgresql.svg' },
-    { skill: 'react', icon: 'react.svg' },
-    { skill: 'spring boot', icon: 'spring.svg' },
-    { skill: 'svelte', icon: 'svelte.svg' },
-    { skill: 'typescript', icon: 'typescript.svg' },
+    { skill: 'angular', icon: 'angular.svg', category: 'frontend' },
+    { skill: 'react', icon: 'react.svg', category: 'frontend' },
+    { skill: 'svelte', icon: 'svelte.svg', category: 'frontend' },
+    { skill: 'javascript', icon: 'javascript.svg', category: 'frontend' },
+    { skill: 'css', icon: 'css3.svg', category: 'frontend' },
+    { skill: 'html', icon: 'html5.svg', category: 'frontend' },
+
+    { skill: 'typescript', icon: 'typescript.svg', category: 'backend' },
+    { skill: 'nodejs', icon: 'nodejs.svg', category: 'backend' },
+    { skill: 'express', icon: 'express.svg', category: 'backend' },
+    { skill: 'go', icon: 'go.svg', category: 'backend' },
+    { skill: 'java', icon: 'java.svg', category: 'backend' },
+    { skill: 'spring boot', icon: 'spring.svg', category: 'backend' },
+    { skill: 'mysql', icon: 'mysql.svg', category: 'backend' },
+    { skill: 'postgresql', icon: 'postgresql.svg', category: 'backend' },
+
+    { skill: 'docker', icon: 'docker.svg', category: 'other' },
+    { skill: 'git', icon: 'git.svg', category: 'other' },
+    { skill: 'github', icon: 'github.svg', category: 'other' },
+    { skill: 'linux', icon: 'linux.svg', category: 'other' },
   ];
+
+  const categories = { frontend: [], backend: [], other: [] };
+
+  const skillsByCategory = skills.reduce((prev, curr) => {
+    return {
+      ...prev,
+      [curr.category]: prev[curr.category].concat(curr),
+    };
+  }, categories);
 </script>
 
-<ul>
-  {#each skills as { skill, icon }}
-    <li class="list-item">
-      <span class="skill">{skill}</span>
-      <span class="icon">
-        <img src="icons/{icon}" alt={skill} width="32" height="32">
-      </span>
-    </li>
-  {/each}
-</ul>
-
-<style>
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  .list-item {
-    display: flex;
-    align-items: center;
-  }
-
-  .skill {
-    color: var(--color-primary);
-  }
-
-  .icon {
-    margin-left: 0.5rem;
-  }
-</style>
+<SkillsList header="Front End Skills" skills={skillsByCategory.frontend} />
+<SkillsList header="Back End Skills" skills={skillsByCategory.backend} />
+<SkillsList header="Other Skills" skills={skillsByCategory.other} />
---
+++ src/lib/components/SkillsList.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let header: string;
+  export let skills: { skill: string; icon: string }[];
+</script>
+
+<h4>{header}</h4>
+<ul>
+  {#each skills as { skill, icon }}
+    <li class="list-item">
+      <span class="icon">
+        <img src="icons/{icon}" alt={skill} width="28" height="28" />
+      </span>
+      <span class="skill">{skill}</span>
+    </li>
+  {/each}
+</ul>
+
+<style>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .list-item {
+    display: flex;
+    align-items: center;
+  }
+
+  .skill {
+    color: var(--color-primary);
+    margin-left: 1rem;
+  }
+
+  .icon {
+    margin-left: 0.5rem;
+  }
+</style>
